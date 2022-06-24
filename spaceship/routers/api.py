fc71163ad5901850bcc0b082dc9365bf8cafb2d9
IvanOmelchenkoIP <io25112002@gmail.com>
2022-06-24 18:07:07 +0300

    Add numpy endpoint to api.py
    Add numpy endpoint to api.py in the folder routers to experiment on the
    Docker image build time and size

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,8 +1,22 @@
+from itertools import product
 from fastapi import APIRouter
+import numpy as np
+import json
 
 router = APIRouter()
-
 
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/matrix')
+def matrix() -> dict:
+    matrix_a = np.random.randint(-20, 20, (10, 10))
+    matrix_b = np.random.randint(-20, 20, (10, 10))
+    product = np.dot(matrix_a, matrix_b)
+
+    matrix_a_ret = json.dumps(matrix_a.tolist())
+    matrix_b_ret = json.dumps(matrix_b.tolist())
+    product_ret = json.dumps(product.tolist())
+
+    return {'matrix_a': matrix_a_ret, 'matrix_b': matrix_b_ret, 'product': product_ret}
